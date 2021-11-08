cfaeec1f59f39892922e334566bce62eef206a14
Vladyslav Proshchavaiev <f3joule@gmail.com>
2021-11-08 16:28:38 +0200

    Optimize migration loop

--- pallets/spaces/src/migrations.rs
+++ pallets/spaces/src/migrations.rs
@@ -6,31 +6,34 @@ use pallet_utils as Utils;
 use super::*;
 
 pub fn fix_corrupted_handles_storage<T: Config>() -> frame_support::weights::Weight {
-  let mut handles_to_remove = Vec::new();
-  let mut handles_iterated = 0;
+    let mut handles_iterated = 0;
+    let mut should_remove;
+    let mut removed = 0;
 
-  for (handle, space_id) in SpaceIdByHandle::iter() {
-    handles_iterated += 1;
+    for (handle, space_id) in SpaceIdByHandle::iter() {
+        handles_iterated += 1;
+        should_remove = false;
 
-    if let Some(space) = Module::<T>::space_by_id(&space_id) {
-      let space_handle_lc = space.handle.map(Utils::Module::<T>::lowercase_handle);
+        if let Some(space) = Module::<T>::space_by_id(&space_id) {
+            let space_handle_lc = space.handle.map(Utils::Module::<T>::lowercase_handle);
 
-      if space_handle_lc.is_none() || space_handle_lc.as_ref() != Some(&handle) {
-        handles_to_remove.push(handle.clone());
-      }
-    } else {
-      handles_to_remove.push(handle.clone());
+            if space_handle_lc.is_none() || space_handle_lc.as_ref() != Some(&handle) {
+                should_remove = true;
+            }
+        } else {
+            should_remove = true;
+        }
+
+        if should_remove {
+            SpaceIdByHandle::remove(handle);
+            removed += 1;
+        }
     }
-  }
 
-  for handle in &handles_to_remove {
-    SpaceIdByHandle::remove(handle);
-  }
+    SpaceIdByHandleStorageFixed::put(true);
 
-  SpaceIdByHandleStorageFixed::put(true);
-
-  T::DbWeight::get().reads_writes(
-    handles_iterated.saturating_mul(2),
-    handles_to_remove.len() as u64 + 1,
-  )
+    T::DbWeight::get().reads_writes(
+        handles_iterated.saturating_mul(2),
+        removed + 1,
+    )
 }
