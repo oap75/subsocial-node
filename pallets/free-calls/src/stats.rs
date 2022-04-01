d2de71dd6d6f5bf696725d1fbe8fe9696473894c
TarekkMA <tarekkma@gmail.com>
2022-04-01 10:27:04 +0200

    rename ConsumerStats to WindowStats

--- pallets/free-calls/src/stats.rs
+++ pallets/free-calls/src/stats.rs
@@ -5,12 +5,12 @@ use crate::config::WindowsConfigSize;
 use crate::quota::NumberOfCalls;
 use scale_info::TypeInfo;
 
-/// A `BoundedVec` that can hold a list of `ConsumerStats` objects bounded by the size of WindowConfigs.
-pub type ConsumerStatsVec<T> = BoundedVec<ConsumerStats<<T as frame_system::Config>::BlockNumber>, WindowsConfigSize<T>>;
+/// A `BoundedVec` that can hold a list of `WindowStats` objects bounded by the size of WindowConfigs.
+pub type WindowStatsVec<T> = BoundedVec<WindowStats<<T as frame_system::Config>::BlockNumber>, WindowsConfigSize<T>>;
 
 /// Keeps track of the executed number of calls per window per consumer (account).
 #[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo)]
-pub struct ConsumerStats<BlockNumber> {
+pub struct WindowStats<BlockNumber> {
     /// The index of this window in the timeline.
     pub timeline_index: BlockNumber,
 
@@ -18,9 +18,9 @@ pub struct ConsumerStats<BlockNumber> {
     pub used_calls: NumberOfCalls,
 }
 
-impl<BlockNumber> ConsumerStats<BlockNumber> {
+impl<BlockNumber> WindowStats<BlockNumber> {
     pub fn new(timeline_index: BlockNumber) -> Self {
-        ConsumerStats {
+        WindowStats {
             timeline_index,
             used_calls: 0,
         }
